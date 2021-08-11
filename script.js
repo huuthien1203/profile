cb0230eda29e5a24c5a5ed3acdddefc1e5d929e4
huuthien1203 <88137574+huuthien1203@users.noreply.github.com>
2021-08-11 16:06:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function() {
+    $('#a1').hover(function() {
+        $('#text').fadeIn();
+
+    }, function() {
+        $('#text').fadeOut();
+    });
+});
+
+$(function() {
+    $('#a2').hover(function() {
+        $('#text1').fadeIn();
+
+    }, function() {
+        $('#text1').fadeOut();
+    });
+});
+$(function() {
+    $('#a3').hover(function() {
+        $('#text2').fadeIn();
+
+    }, function() {
+        $('#text2').fadeOut();
+    });
+});
